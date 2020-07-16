3c50dd3c94938beb03d703bbb002f93b77f5a4d9
xushuaibing <2368184978@qq.com>
2020-07-16 10:52:45 +0800

    Update install_devtools.sh
    remove ca-certificates

--- docker-custom/scripts/install_devtools.sh
+++ docker-custom/scripts/install_devtools.sh
@@ -4,7 +4,7 @@ set -ex
 # Installing Devtools
 if [[ ${TAG_SUFFIX} != "minimal" ]]; then
   echo "Installing devtools"
-  apk add --no-cache --virtual devtools build-base linux-headers udev python python3 ca-certificates ffmpeg
+  apk add --no-cache --virtual devtools build-base linux-headers udev python python3 ffmpeg
 else
   echo "Skip installing devtools"
 fi
